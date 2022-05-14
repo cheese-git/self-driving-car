8b7847e89627e6a7e148cdfc4f1235414f2520f4
cheese <cheese-h@qq.com>
2022-05-14 23:16:22 +0800

    feat: move like a real car

--- car.class.mjs
+++ car.class.mjs
@@ -4,6 +4,7 @@ class Car {
   constructor(x, y, width, height) {
     this.x = x
     this.y = y
+    this.angle = 0
     this.width = width
     this.height = height
     this.speed = 0
@@ -15,6 +16,20 @@ class Car {
   }
 
   update() {
+    this.#move()
+  }
+
+  draw(ctx) {
+    ctx.save()
+    ctx.translate(this.x, this.y)
+    ctx.rotate(-this.angle)
+    ctx.rect(- this.width / 2, - this.height / 2, this.width, this.height)
+    ctx.fillStyle = this.color
+    ctx.fill()
+    ctx.restore()
+  }
+
+  #move() {
     if (this.controls.forward && this.speed < this.maxSpeed) {
       this.speed += this.acceleration
     }
@@ -25,13 +40,15 @@ class Car {
     if (this.speed > 0) this.speed = Math.max(this.speed - this.friction, 0)
     if (this.speed < 0) this.speed = Math.min(this.speed + this.friction, 0)
 
-    this.y -= this.speed
-  }
+    if (this.speed !== 0) {
+      const flip = this.speed > 0 ? 1 : -1
+      if (this.controls.left) this.angle += 0.02 * flip
+      if (this.controls.right) this.angle -= 0.02 * flip
+    }
 
-  draw(ctx) {
-    ctx.rect(this.x - this.width / 2, this.y - this.height / 2, this.width, this.height)
-    ctx.fillStyle = this.color
-    ctx.fill()
+
+    this.y -= this.speed * Math.cos(this.angle)
+    this.x -= this.speed * Math.sin(this.angle)
   }
 }
 
