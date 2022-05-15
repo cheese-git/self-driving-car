7e72187b96fd0e066f50d527b42a96b10aec6eda
cheese <cheese-h@qq.com>
2022-05-15 10:07:02 +0800

    feat: draw road

--- main.mjs
+++ main.mjs
@@ -1,18 +1,21 @@
 import Car from './car.class.mjs'
+import Road from './road.class.mjs'
 
 const canvas = document.getElementById('my-canvas')
 canvas.width = 200
 canvas.height = window.innerHeight
 
 const ctx = canvas.getContext('2d')
-
+const road = new Road(100, canvas.width * 0.9)
 const car = new Car(100, 200, 30, 50)
+
 
 animate()
 
 function animate() {
   canvas.height = window.innerHeight
   car.update()
+  road.draw(ctx)
   car.draw(ctx)
   requestAnimationFrame(animate)
 }
---
+++ road.class.mjs
@@ -0,0 +1,25 @@
+export default class Road {
+  constructor(x, width, laneCount = 3) {
+    this.x = x
+    this.width = width
+    this.laneCount = laneCount
+
+    this.left = this.x - this.width / 2
+    this.right = this.x + this.width / 2
+
+    const infinity = 1000000
+    this.top = -infinity
+    this.bottom = infinity
+  }
+
+  draw(ctx) {
+    ctx.strokeStyle = 'white'
+    ctx.lineWidth = 5
+
+    ctx.moveTo(this.left, this.top)
+    ctx.lineTo(this.left, this.bottom)
+    ctx.moveTo(this.right, this.top)
+    ctx.lineTo(this.right, this.bottom)
+    ctx.stroke()
+  }
+}
