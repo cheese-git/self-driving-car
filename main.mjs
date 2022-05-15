0931b04957c6c62482a9ce4170684cd96f951002
cheese <cheese-h@qq.com>
2022-05-15 22:03:55 +0800

    feat: paralize training

--- main.mjs
+++ main.mjs
@@ -14,11 +14,7 @@ const carCtx = carCanvas.getContext('2d')
 const neuralCtx = neuralCanvas.getContext('2d')
 
 const road = new Road(100, carCanvas.width * 0.9)
-const heroCar = new HeroCar({
-  x: road.getLaneCenter(1),
-  y: 100,
-  color: 'blue'
-})
+
 const traffic = [
   new Car({
     x: road.getLaneCenter(1),
@@ -27,24 +23,41 @@ const traffic = [
   })
 ]
 
+const N = 100
+const heroCars = []
+for (let i = 0; i < N; i++) {
+  heroCars.push(new HeroCar({
+    x: road.getLaneCenter(1),
+    y: 100,
+    color: 'blue'
+  }))
+}
+
+let bestHeroCar = heroCars[0]
+
 
 animate()
 
 function animate(time) {
   carCanvas.height = window.innerHeight
   neuralCanvas.height = window.innerHeight
-  heroCar.update(road.borders, traffic)
-  traffic.forEach(car => car.update(road.borders, [heroCar]))
+  heroCars.forEach(heroCar => heroCar.update(road.borders, traffic))
+  traffic.forEach(car => car.update(road.borders, []))
+
+  bestHeroCar = heroCars.find(car => car.y === Math.min(...heroCars.map(car => car.y)))
 
   carCtx.save()
-  carCtx.translate(0, -heroCar.y + carCanvas.height * 0.7)
-  road.draw(carCtx)
+  carCtx.translate(0, -bestHeroCar.y + carCanvas.height * 0.7)
   traffic.forEach(car => car.draw(carCtx))
-  heroCar.draw(carCtx)
+  heroCars.forEach(heroCar => {
+    road.draw(carCtx)
+    heroCar.draw(carCtx)
+  })
   carCtx.restore()
-  
+
+
   neuralCtx.lineDashOffset = - time / 80
-  Visualizer.drawNetwork(neuralCtx, heroCar.brain)
-  
+  Visualizer.drawNetwork(neuralCtx, bestHeroCar.brain)
+
   requestAnimationFrame(animate)
 }
